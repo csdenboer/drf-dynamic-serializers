a7c43433e84c1142d3bdb0f1bb067d1bdc0c24db
Christian den Boer <csdenboer@Christians-MacBook-Pro.local>
2020-08-19 15:03:22 +0200

    updated docs conf.py: changed insert path (attempt to fix readthedocs.io build)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,9 +12,10 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../'))
 
 import django
+
+sys.path.insert(0, os.path.abspath('../..'))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tests.settings")
 django.setup()
